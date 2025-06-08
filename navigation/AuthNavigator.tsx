e9bfb460d0554ef32288daf36c47dc87a3f77ff8
MarcoCaamal <caamalmarco99@gmail.com>
2025-06-08 15:12:18 -0500

    Se quito  el uso de expo

--- navigation/AuthNavigator.tsx
+++ navigation/AuthNavigator.tsx
@@ -4,11 +4,13 @@ import { Text, View } from 'react-native';
 
 import LoginScreen from '../modules/auth/screens/LoginScreen';
 import RegisterScreen from '../modules/auth/screens/RegisterScreen';
+import WifiScanScreen from '../modules/devices/screens/WifiScanScreen';
 
 export type AuthStackParamList = {
   Login: undefined;
   Register: undefined;
   Test: undefined;
+  WifiScan: undefined;
 };
 
 const Stack = createStackNavigator<AuthStackParamList>();
@@ -25,9 +27,10 @@ function TestScreen() {
 export default function AuthNavigator() {
   return (
     <Stack.Navigator 
-      initialRouteName="Login" 
+      initialRouteName="WifiScan" 
       screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name="WifiScan" component={WifiScanScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
